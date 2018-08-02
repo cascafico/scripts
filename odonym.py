b5a6e7a922f25795752e363c9343c10520a3673b
cascafico <cascafico@gmail.com>
2018-08-02 13:59:40 +0200

    added bot telegram

---
+++ odonym.py
@@ -0,0 +1,32 @@
+import os
+import time
+import datetime
+import telepot
+import subprocess
+
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    odonym = msg['text']
+
+    print 'Got odonym: %s' % odonym
+    now=datetime.datetime.now()
+    csvname = "/tmp/" + odonym.replace(" ", "") + ".csv"
+
+    query = "cat /home/pi/git/scripts/???_names.csv | grep \"" + odonym + "\" > " + csvname                    
+    addheader = "sed -i '1 i\odonym,lat,lon' " + csvname
+
+    print 'Built command: %s' % query
+    bot.sendMessage(chat_id, "Fetching {} from italian highway names...".format(odonym))
+    os.system(query)
+    matches = subprocess.check_output("wc -l {} | cut -d\" \" -f1".format(csvname), shell=True)
+    bot.sendMessage(chat_id, "Found {}".format(matches))
+    os.system(addheader)
+    bot.sendDocument(chat_id, document=open(csvname, 'rb'))
+
+bot = telepot.Bot(Ã'place your token here')
+bot.message_loop(handle)
+print 'I am listening ...'
+
+while 1:
+    time.sleep(4)
